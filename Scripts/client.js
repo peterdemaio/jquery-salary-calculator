f050260e3174e34c3bfd66d440555489586fa537
Peter DeMaio <pedemaio@gmail.com>
2019-12-13 15:46:08 -0600

    made table that accepts emp input and appends to DOM

--- Scripts/client.js
+++ Scripts/client.js
@@ -4,5 +4,34 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('JQUERY READY');
+    $('#addEmployeeButton').on('click', addEmployee);
+}
+
+function addEmployee(){
+    console.log('Employee Add Button Clicked')
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let idNum = $('#idNumIn').val();
+    let jobTitle = $('#jobTitleIn').val();
+    let annualSalary = $('#annualSalaryIn').val();
     
+    console.log(firstName, lastName, idNum, jobTitle, annualSalary);
+
+    $('#workerList').append(
+        `
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNum}</td>
+            <td>${jobTitle}</td>
+            <td>${annualSalary}</td>
+            <td><button class="deleteButton btn btn-danger">DELETE</button</td>
+        </tr>
+        `
+    )
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idNumIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
 }
