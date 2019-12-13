cea06c34f803c06b1e7aabb99fdf9d7f357cb503
Peter DeMaio <pedemaio@gmail.com>
2019-12-13 16:52:27 -0600

    finished base mode, need to add Css and begin stretch mode

--- Scripts/client.js
+++ Scripts/client.js
@@ -8,29 +8,42 @@ function readyNow() {
     $('#employeeList').on('click', '.deleteButton', deleteEmployee);
 }
 
-function addEmployee(){
-    console.log('Employee Add Button Clicked')
+
+let employeeListArray = [];
+
+function addEmployee() {
     let firstName = $('#firstNameIn').val();
     let lastName = $('#lastNameIn').val();
-    let idNum = $('#idNumIn').val();
+    let idNum = Number($('#idNumIn').val());
     let jobTitle = $('#jobTitleIn').val();
-    let annualSalary = $('#annualSalaryIn').val();
+    let annualSalary = Number($('#annualSalaryIn').val());
     
-    annualSalary = Number(annualSalary);
+    console.log('Employee Add Button Clicked')
+    if (
+        firstName.length === 0 ||
+        lastName.length === 0 ||
+        idNum === 0 ||
+        jobTitle.length === 0 ||
+        annualSalary === 0) {
+        alert('Some Information is missing, please try again.')
+    } else {
+        $('#employeeList').append(
+            `
+            <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${idNum}</td>
+                <td>${jobTitle}</td>
+                <td>${annualSalary}</td>
+                <td><button class="deleteButton btn btn-danger">DELETE</button</td>
+            </tr>
+            `
+        )
+    }
     console.log(firstName, lastName, idNum, jobTitle, annualSalary);
+    makeEmployeeArray(firstName, lastName, idNum, jobTitle, annualSalary);
+    showSalary();
 
-    $('#employeeList').append(
-        `
-        <tr>
-            <td>${firstName}</td>
-            <td>${lastName}</td>
-            <td>${idNum}</td>
-            <td>${jobTitle}</td>
-            <td>${annualSalary}</td>
-            <td><button class="deleteButton btn btn-danger">DELETE</button</td>
-        </tr>
-        `
-    )
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
     $('#idNumIn').val('');
@@ -41,4 +54,29 @@ function addEmployee(){
 function deleteEmployee() {
     console.log('Delete Employee Button Clicked')
     $(this).closest('tr').remove();
+}
+
+function makeEmployeeArray(fname, lname, inum, jtitle, asalary)  {
+    const oneEmployee = {
+    firstname: fname,
+    lastname: lname,
+    IDnum: inum,
+    jobtitle: jtitle ,
+    annualSalary: asalary
+    };
+    employeeListArray.push(oneEmployee);
+    console.log(employeeListArray);
+}
+function showSalary() {
+    let totalSalary = 0;
+    for (let i =0; i < employeeListArray.length; i++) {
+        totalSalary +=employeeListArray[i].annualSalary;
+    } let counter = $( '#monthlySalaryOut');
+    counter.empty();
+    let monthlySalary = Math.round(totalSalary / 12);
+    if (monthlySalary > 20000) {
+    counter.append(`<h4 class="negative">Monthly Salary: $${monthlySalary}</h4>`)
+}   else {
+    counter.append(`<h4 class="positive">Monthly Salary: $${monthlySalary}</h4>`)
+}
 }
